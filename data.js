e4d0fcadd1eaadea41106ef88b78f17108d53059
Kailane da Silva Ribeiro <dasilvaribeirokailane@gmail.com>
2024-08-19 09:02:50 -0300

    Update data.js

--- data.js
+++ data.js
@@ -3,7 +3,6 @@ let baralhos = [{id:1, nome:'thiagão'},
 ]
 
 let flashcards = [{id: 1, pergunta: 'qual sua musica favorita do thiagão?', resposta: 'O amanha vai ser melhor', idBaralho: 1 },
-                  {id: 2, pergunta: 'qual sua musica favorita do Ton Carfi?', resposta: 'Hospicio', idBaralho: 2 }
-                 ]
+                  {id: 2, pergunta: 'qual sua musica favorita do Ton Carfi?', resposta: 'Hospicio', idBaralho: 2 }]
 
 module.exports = {baralhos, flashcards}
