e92bb5f519ed770e4db9e5632de39cce8b8bd411
Kailane da Silva Ribeiro <dasilvaribeirokailane@gmail.com>
2024-08-19 08:26:57 -0300

    Update data.js

--- data.js
+++ data.js
@@ -2,6 +2,8 @@ let baralhos = [{id:1, nome:'thiagão'},
 {id:2, nome:'Ton Carfi'}
 ]
 
-let flashcards = [{}]
+let flashcards = [{id: 1, pergunta: 'qual sua musica favorita do thiagão?', resposta: 'O amanha vai ser melhor', idBaralho: 1 },
+                  {id: 2, pergunta: 'qual sua musica favorita do Ton Carfi?', resposta: 'Hospicio', idBaralho: 2 }
+                 ]
 
 module.exports = {baralhos, flashcards}
